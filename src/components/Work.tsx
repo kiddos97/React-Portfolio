c82ccb453176f2a7288ca6b9abc41e57379de482
Emmanuel <osaroimarhiagbe@gmail.com>
2024-01-03 17:02:56 -0600

    adding background image in work.tsx

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,5 +1,6 @@
 import work from "../assets/workImg.jpeg";
 import realestate from "../assets/realestate.jpg";
+
 const Work = () => {
   return (
     <div name="work" className="w-full md:h-screen bg-[#0a192f] text-gray-300">
@@ -8,20 +9,30 @@ const Work = () => {
           <p className="text-4xl font-bold inline border-b-4 border-pink-600">
             Work
           </p>
-          <p className="py-6"> //Check out some of my recent work</p>
+          <p className="py-6">Check out some of my recent work</p>
         </div>
-        <div className="gird sm: gird-cols-2 md: grid-cols-3 gap-4">
+
+        <div
+          style={{ backgroundImage: `url(${work})` }}
+          className="gird sm: gird-cols-2 md: grid-cols-3 gap-4"
+        >
           <div className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
             {/* hover effects */}
-            <div>
+            <div className="opacity-0 group-hover: opacity-100">
               <div>
-                <span></span>
-                <div>
+                <span className="text-2xl font-bol text-white tracking-wider">
+                  REACT JS Application
+                </span>
+                <div className="pt-8 text-center">
                   <a href="/">
-                    <button></button>
+                    <button className="text-cener rounded-lg px-4 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Demo
+                    </button>
                   </a>
                   <a href="/">
-                    <button></button>
+                    <button className="text-cener rounded-lg px-4 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Code
+                    </button>
                   </a>
                 </div>
               </div>
